11883779c551789d1c9ae919a2eb23a79d865cbc
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-22 15:04:07 +0530

    Create bitbyte.sv

---
+++ Data Types/Casting/Bit_Byte/bitbyte.sv
@@ -0,0 +1,11 @@
+module casting_bit_byte();
+ //create variable bit and byte type
+ byte byte_data;
+ bit [3:0] bit_data;
+ initial begin
+   bit_data = 4'b1011; 
+   $display("\nbefore casting bit_data = %0b",bit_data);
+   byte_data = byte'(bit_data); 
+   $display("after casting bit to byte = %b\n",byte_data);
+ end
+endmodule
