400c4cfa74e95ead9640fa33f5b094c9fa78873e
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-22 15:14:59 +0530

    Create realtime.sv

---
+++ Data Types/Casting/Realtime/realtime.sv
@@ -0,0 +1,10 @@
+module casting_real_time(); 
+ real real_data;
+ time time_data;
+ initial begin
+   real_data = 8.06;  
+   $display("\nbefore casting real_data = %0.2f",real_data);
+   time_data = time'(real_data); 
+   $display("after casting real to time = %0t\n",time_data);
+ end
+endmodule 
