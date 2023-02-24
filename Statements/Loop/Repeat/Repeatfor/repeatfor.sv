19f5cfb9782265c76bbaba433bd3fe92dda8bd9f
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-24 22:47:29 +0530

    Create repeatfor.sv

---
+++ Statements/Loop/Repeat/Repeatfor/repeatfor.sv
@@ -0,0 +1,9 @@
+module repeat_for;
+initial begin
+  for (int i = 1;i<=4;i++)begin   // for loop initialization, repeat the statements inside it for  
+    $display ("Good morning");   // 4 times (i =1,2,3,4)
+    $display ("Keep Shining");
+    $display ("------------");
+  end 
+end 
+endmodule 
