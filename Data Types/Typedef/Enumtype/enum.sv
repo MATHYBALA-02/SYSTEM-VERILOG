f46b3e2647206ba7833ab239cb4dac3b439919e2
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-26 01:13:33 +0530

    Create enum.sv

---
+++ Data Types/Typedef/Enumtype/enum.sv
@@ -0,0 +1,18 @@
+module enumtype;
+
+//Use typedef to create enum type
+typedef enum {RORITO, FLAIRFX, REYNOLDS} e_pen;
+
+initial begin
+
+//Create  a variable use as enum datatype
+e_pen pen;
+pen = RORITO;
+
+//displaying the value of pen
+$display("\n typedef enum example");
+$display("\n pen = %s",pen);
+$display("");
+
+end
+endmodule
