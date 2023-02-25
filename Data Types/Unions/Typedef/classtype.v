c87d5b5c5ee39d9d9a241fb239bf7d212129938f
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-26 01:01:34 +0530

    Update and rename typedefn.v to classtype.v

---
+++ Data Types/Unions/Typedef/classtype.v
@@ -0,0 +1,26 @@
+typedef class fruit2;
+
+//class1 declaration
+class fruit1; 
+fruit2 f ;  // 	Without typedef fruit2 declaration cause compile error.
+endclass
+
+//class2 declaration 
+class fruit2;
+fruit1 f ;
+endclass
+
+module typedefclass;
+
+initial begin
+fruit1 class1;
+fruit2 class2;
+
+//Display the values
+$display("\n typedef class example");
+$display("\n fruit2 class usage with typedef to avoid compile error");
+$display("");
+
+end
+
+endmodule
