f45bd1b1c7d050e5c8d08bfe6a8ce7b2219031b5
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-27 22:37:33 +0530

    Create case.sv

---
+++ Statements/Condition/Case/Unique/Case/case.sv
@@ -0,0 +1,16 @@
+module u1;
+bit [1:0] x;
+initial begin
+  x = 2'b01;
+
+//Here no condition is true, default statement will get executed.
+  unique case(x)
+  00 : $display(" Value of x is = %0b", x);
+  // 01 : $display(" Value of x is = %0b", x);
+  10 : $display(" Value of x is = %0b", x);
+  11 : $display(" Value of x is = %0b", x);
+  default : $display(" Value of x is = %0b", x);
+
+endcase
+end
+endmodule
