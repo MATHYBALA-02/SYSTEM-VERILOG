38bc75153edc4c9fa7e948ca4cc705a49ff008d5
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-28 09:51:51 +0530

    Create inst.sv

---
+++ Classes/Constantvariables/Instance/inst.sv
@@ -0,0 +1,35 @@
+class data;
+  const int a;
+  string b;
+  
+  function new();
+    a=5;
+    b="bhavana";
+  endfunction:new
+  
+  function void display();
+    $display("a=%0d,b=%0d",a,b);
+  endfunction:display
+
+endclass:data
+
+module instance_class;
+  data t1;
+
+ 
+  initial begin:BEGIN_I
+    t1=new();
+    //t1.a=2;
+    //-------------------------------------------------------
+    // Here we should not assign value for the 'a' because 
+    // we declared the 'a' as constant variable.
+    // so if we assign any value to the 'a' it throws errors 
+    // it won't take any value eventhough we declared.
+    //-------------------------------------------------------
+    t1.b="bjt";
+    $display("");
+    t1.display();
+    $display("");
+  end:BEGIN_I
+
+endmodule
